f4e7d3a072f121aa71716864deff91f9797017ac
PancratiA <pancrati.adriana-maria.22@stud.umfst.ro>
2023-10-31 18:18:07 +0200

    Adaugare Clase

--- Test/Intrebare.java
+++ Test/Intrebare.java
@@ -1,4 +1,51 @@
 package Test;
 
 public abstract class Intrebare {
+    private String intrebare;
+    private String raspunsCorect;
+
+
+    public Intrebare(String intrebare, String raspunsCorect) {
+        this.intrebare = intrebare;
+        this.raspunsCorect=raspunsCorect;
+    }
+
+    public Intrebare(){
+        this("","");
+    }
+
+    // Metoda abstracta
+    public abstract boolean verificaRaspuns(String raspuns);
+
+    @Override
+    public String toString() {
+        return "Intrebare: " + intrebare;
+    }
+
+    public String adaugareIntrebareFisier(){
+
+        return intrebare;
+
+    }
+    public String adaugareRaspunsCorectFisier(){
+
+        return raspunsCorect;
+
+    }
+
+    public String getIntrebare() {
+        return intrebare;
+    }
+
+    public void setIntrebare(String intrebare) {
+        this.intrebare = intrebare;
+    }
+
+    public String getRaspunsCorect() {
+        return raspunsCorect;
+    }
+
+    public void setRaspunsCorect(String raspunsCorect) {
+        this.raspunsCorect = raspunsCorect;
+    }
 }
--- Test/Test.java
+++ Test/Test.java
@@ -1,4 +1,28 @@
 package Test;
+import java.util.ArrayList;
 
 public class Test {
+    private String materie;
+    private ArrayList<Intrebare> intrebari;
+
+    public Test() {
+        intrebari = new ArrayList<>();
+    }
+
+    public void addIntrebare(Intrebare intrebare) {
+        intrebari.add(intrebare);
+    }
+
+    public ArrayList<Intrebare> getIntrebari() {
+        return intrebari;
+    }
+
+    public String getMaterie() {
+        return materie;
+    }
+
+    public void setMaterie(String materie) {
+        this.materie = materie;
+    }
 }
+
